fbb50fd220db715739d2522a52e854c8408d4c13
Marcos Ramos <marcos.ramos@estudiante.uam.es>
2022-07-01 13:01:47 +0200

    Commented compile.bat

--- Tools/compile.bat
+++ Tools/compile.bat
@@ -1,9 +1,16 @@
-:: Compile processors and annotations
+:: Standart mode: Compile all processors and annotations, use build folder as source for processing main
+:: 1. Compile processors and annotations
 @javac src\processors\*.java src\annotations\*.java -d build
-:: Create .jar file
-@jar -c -f lib\processorlib.jar build/processors/*.class build/annotations/*.class
-:: Compile main package using library
+:: 2. Compile main package using build
 @javac -proc:only -cp build -processor processors.RootProcessor src\main\*.java
-:: Visualize output
-@del /q class_diagram.png
+:: 3. Visualize output
 @dot -Tpng class_diagram.dot -o class_diagram.png
+
+:: Create .jar file
+:: @jar -c -f lib\processorlib.jar build/processors/*.class build/annotations/*.class
+
+:: Library mode: Use library as source for processing main
+:: 1. Compile main package using library
+:: @javac -proc:only -cp lib\processorlib.jar -processor processors.RootProcessor src\main\*.java
+:: 2. Visualize output
+:: @dot -Tpng class_diagram.dot -o class_diagram.png
